36fd6bfb20f7423a5176279ee5a095657a213a00
Denisodua <denisodua@gmail.com>
2024-11-19 14:24:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,11 @@ app.get('/', (req, res) => {
 app.post('/api/proxy/campaigns', async (req, res) => {
   try {
     const response = await axios.post(
-      'https://partner.onlytraffic.com/api/marketer?do=campaigns', 
+      'https://partner.onlytraffic.com/api/marketer?do=campaigns?api_auth_key=55596-DB771-EF2E1-7372E', 
       {},
       {
         headers: {
           'Content-Type': 'application/json',
-          'api_auth_key': '55596-DB771-EF2E1-7372E'
         }
       }
     );
